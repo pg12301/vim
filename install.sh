0f801e0da6eb69d88a6996b04e09ec12fa8c6172
gang.pei <gang.pei@seraphic-corp.com>
2020-06-04 18:14:05 +0800

    vim config

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-mv ~/.vimrc ~/.vimrc_bak
-mv ~/.vim ~/.vim_bak
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cd ~/.vim/bundle/
+if [ -d $HOME/.config/nvim ]
+then
+  mv ~/.config/nvim $HOME/.config/nvim_bak
+fi
+mkdir -p $HOME/.config/nvim/autoload $HOME/.config/nvim/bundle
+curl -LSso $HOME/.config/nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd $HOME/.config/nvim/bundle/
+git clone https://github.com/Yggdroot/indentLine.git
 git clone https://github.com/chxuan/change-colorscheme
 git clone https://github.com/chxuan/vim-buffer
 git clone https://github.com/jiangmiao/auto-pairs
@@ -13,12 +16,11 @@ git clone https://github.com/octol/vim-cpp-enhanced-highlight
 git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes
 git clone https://github.com/terryma/vim-smooth-scroll
-git clone https://github.com/tomtom/tlib_vim.git
-git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
-git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/preservim/nerdcommenter.git
 git clone https://github.com/honza/vim-snippets.git
-git clone https://github.com/jayli/vim-easycomplete.git
 git clone https://github.com/jayli/vim-dictionary.git
+git clone https://github.com/preservim/tagbar.git
+git clone https://github.com/luochen1990/rainbow.git
 cd -
-cp -a .vimrc ~/.vimrc
-cp -a colors ~/.vim/colors
+cp -a init.vim $HOME/.config/nvim/init.vim
+cp -a colors $HOME.config/nvim/colors
