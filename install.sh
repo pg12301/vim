4f1784a1520343e7180f1851d3f81b99300a9d4a
gang <1351581995@qq.com>
2023-04-26 13:10:38 +0800

    update

--- install.sh
+++ install.sh
@@ -7,21 +7,21 @@ fi
 mkdir -p $HOME/.config/nvim/autoload $HOME/.config/nvim/bundle
 cp -a pathogen.vim $HOME/.config/nvim/autoload
 cd $HOME/.config/nvim/bundle/
-git clone git://github.com/Yggdroot/indentLine.git
-git clone git://github.com/chxuan/change-colorscheme
-git clone git://github.com/chxuan/vim-buffer
-git clone git://github.com/jiangmiao/auto-pairs
-git clone git://github.com/scrooloose/nerdtree
-git clone git://github.com/octol/vim-cpp-enhanced-highlight
-git clone git://github.com/vim-airline/vim-airline
-git clone git://github.com/vim-airline/vim-airline-themes
-git clone git://github.com/terryma/vim-smooth-scroll
-git clone git://github.com/preservim/nerdcommenter.git
-git clone git://github.com/honza/vim-snippets.git
-git clone git://github.com/jayli/vim-dictionary.git
-git clone git://github.com/preservim/tagbar.git
-git clone git://github.com/luochen1990/rainbow.git
-git clone git://github.com/junegunn/fzf.vim.git
+git clone https://github.com/Yggdroot/indentLine
+git clone https://github.com/chxuan/change-colorscheme
+git clone https://github.com/chxuan/vim-buffer
+git clone https://github.com/jiangmiao/auto-pairs
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/octol/vim-cpp-enhanced-highlight
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
+git clone https://github.com/terryma/vim-smooth-scroll
+git clone https://github.com/preservim/nerdcommenter
+git clone https://github.com/honza/vim-snippets
+git clone https://github.com/jayli/vim-dictionary
+git clone https://github.com/preservim/tagbar
+git clone https://github.com/luochen1990/rainbow
+git clone https://github.com/junegunn/fzf.vim
 cd -
 cp -a init.vim $HOME/.config/nvim/init.vim
 cp -a colors $HOME/.config/nvim
